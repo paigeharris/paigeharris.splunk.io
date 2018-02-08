808357af96f0b0c51cda341b1591674f94c980aa
blakeharris <harrisblake85@gmail.com>
2018-02-08 02:53:53 -0700

    add more projects

--- src/Projects.js
+++ src/Projects.js
@@ -3,6 +3,18 @@ import Project from './Project.js'
 
 const Projects = (props) => {
   let projects = [
+    {
+      name : "Mile High Drone",
+      roles: "Full Stack Solo",
+      img:"https://i.imgur.com/rHUuImb.png",
+      desc:"Website for a Denver drone service, contact, gallery, etc",
+      technologies :["NodeJs","React","Express","JS","HTML","CSS"],
+      live_link:"http://www.blakeharris.com/dronestore/",
+      live_repo:"https://github.com/harrisblake85/dronestore",
+      backend_link:"https://tooliebox-api.herokuapp.com/",
+      backend_repo:"https://github.com/harrisblake85/tooliebox_api",
+      sep_backend:true
+    },
   {
     name : "ToolieBox",
     roles: "Backend, Tool & Tag",
@@ -11,8 +23,20 @@ const Projects = (props) => {
     technologies :["Ruby on Rails","Angular","Angular Materials","JS","HTML","CSS"],
     live_link:"https://tooliebox.herokuapp.com/",
     live_repo:"https://github.com/tg970/tooliebox_frontend",
-    backend_link:"https://tooliebox.herokuapp.com/",
+    backend_link:"https://tooliebox-api.herokuapp.com/",
     backend_repo:"https://github.com/harrisblake85/tooliebox_api",
+    sep_backend:true
+  },
+  {
+    name : "Creatives For A Cause",
+    roles: "Full Stack Solo",
+    img:"https://i.imgur.com/aUuRkOY.png",
+    desc:"T-Shirt Commerce Store With User Submitted Designs!",
+    technologies :["NodeJs","Express","React","JS","HTML","CSS"],
+    live_link:"https://creativesforacause.herokuapp.com/",
+    live_repo:"https://github.com/harrisblake85/cfac-react",
+    backend_link:"https://cfac-api.herokuapp.com/",
+    backend_repo:"https://github.com/harrisblake85/creativesforacause_api",
     sep_backend:true
   },
   {
@@ -41,8 +65,8 @@ const Projects = (props) => {
     img:"https://i.imgur.com/jhzY3Cz.png",
     desc:"Compete Against The AI In A Game To Become Rich",
     technologies :["JS","HTML","CSS","JQuery"],
-    live_link:"https://harrisblake85.github.io/Economics_The_Game/",
-    live_repo:"https://github.com/harrisblake85/Economics_The_Game",
+    live_link:"http://www.blakeharris.com/thegame/",
+    live_repo:"https://github.com/harrisblake85/thegame",
     sep_backend:false
   }
 ];
