8961a5f1ec2f71af3adeb6879692cc5dd5bda13b
blakeharris <harrisblake85@gmail.com>
2018-10-26 04:59:43 -0600

    changed me picture

--- src/About.js
+++ src/About.js
@@ -7,7 +7,7 @@ const About = () => {
 
       <span className="topspan">Oh Me?</span>
 
-      <img src="https://i.imgur.com/6tZcBgr.png" alt="me"></img>
+      <img src="https://i.imgur.com/KRocURT.jpg" alt="me"></img>
       <h3>Well it all starts with a young lorem. That lorem being me: Lorem Ipsum</h3>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi euismod, justo sit amet imperdiet ultricies, nisi nunc pulvinar orci, id ullamcorper eros dui eget sem. Pellentesque tristique sed risus eget pretium. Fusce commodo vestibulum nisl, ac rhoncus velit. Donec et dignissim leo. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Phasellus vitae tincidunt tortor, ut ultrices ligula. Donec ac ligula ut odio finibus bibendum. Pellentesque finibus sem nec imperdiet volutpat.</p>
     </div>
