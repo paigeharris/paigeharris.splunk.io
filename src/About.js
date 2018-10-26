1cf8514aa1bcb60b0ffec04f85c44f9a620d28bc
blakeharris <harrisblake85@gmail.com>
2018-10-26 05:18:54 -0600

    test

--- src/About.js
+++ src/About.js
@@ -9,7 +9,7 @@ const About = () => {
 
       <img className="mepic" src="https://i.imgur.com/mkiP667.jpg" alt="me"></img>
       <h3>Well it all starts with a young lorem. That lorem being me: Lorem Ipsum</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi euismod, justo sit amet imperdiet ultricies, nisi nunc pulvinar orci, id ullamcorper eros dui eget sem. Pellentesque tristique sed risus eget pretium. Fusce commodo vestibulum nisl, ac rhoncus velit. Donec et dignissim leo. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Phasellus vitae tincidunt tortor, ut ultrices ligula. Donec ac ligula ut odio finibus bibendum. Pellentesque finibus sem nec imperdiet volutpat.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi euismod, justo sit amet imperdiet ultricies, nisi nunc pulvinar orci, id ullamcorper eros dui eget sem. Pellentesque tristique sed risus eget pretium. Fusce commodo vestibulum nisl, ac rhoncus velit. Donec et dignissim leo. Vestibulum ante ium primis in faucibus orci luctus et ultrices posuere cubilia Curae; Phasellus vitae tincidunt tortor, ut ultrices ligula. Donec ac ligula ut odio finibus bibendum. Pellentesque finibus sem nec imperdiet volutpat.</p>
     </div>
 
     </div>
